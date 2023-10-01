b6223982e47cd732814b17266bb7c6200434c1d0
davigvalerio <al.davigvalerio@gmail.com>
2023-10-01 02:21:25 -0300

    refactor(pulls): show just necessary data

--- src/github/get-pulls.ts
+++ src/github/get-pulls.ts
@@ -1,9 +1,12 @@
+import { getMonth, parseISO } from 'date-fns';
 import { Octokit } from 'octokit';
 
 import config from '../../config';
 
 (async (): Promise<void> => {
   const octokit = new Octokit({ auth: config.github.token });
+
+  const user = await octokit.rest.users.getAuthenticated();
 
   config.github.repositories.map(async ({ name }, index) => {
     if (index !== 0) return;
@@ -17,53 +20,80 @@ import config from '../../config';
       state: 'all',
     });
 
-    console.table(
-      pulls.data.map((r) => ({
-        number: r.number,
-        state: r.state,
-        title: r.title,
-        body: typeof r.body === 'string' ? r.body.substring(0, 64) : r.body,
-        created_at: r.created_at,
-        updated_at: r.updated_at,
-      }))
-    );
+    const fPulls = pulls.data.map((r) => ({
+      number: r.number,
+      state: r.state,
+      user: r.user.login,
+      title: r.title,
+      created_at: r.created_at,
+      updated_at: r.updated_at,
+    }));
 
-    const comments = await octokit.request(
-      'GET /repos/{owner}/{repo}/pulls/{pull_number}/comments',
-      {
-        owner,
-        repo,
-        pull_number: 71,
-        per_page: 100,
+    const promise = fPulls.map(async (pr) => {
+      const comments = await octokit.request(
+        'GET /repos/{owner}/{repo}/pulls/{pull_number}/comments',
+        {
+          owner,
+          repo,
+          pull_number: pr.number,
+          per_page: 100,
+        }
+      );
+
+      const reviews = await octokit.request(
+        'GET /repos/{owner}/{repo}/pulls/{pull_number}/reviews',
+        {
+          owner,
+          repo,
+          pull_number: pr.number,
+          per_page: 100,
+        }
+      );
+
+      return {
+        title: `[${pr.number}] - ${pr.title} @${pr.user} (${new Date(
+          pr.updated_at
+        ).toLocaleString('pt-rr')}) ${
+          getMonth(parseISO(pr.updated_at)) === 8 ? 'Current!' : ''
+        }`,
+        reviews: reviews.data
+          .filter((r) => r.user.login === user.data.login)
+          .filter((r) => getMonth(parseISO(r.submitted_at)) === 8),
+        comments: comments.data
+          .filter((r) => r.user.login === user.data.login)
+          .filter(
+            (r) =>
+              getMonth(parseISO(r.updated_at)) === 8 ||
+              getMonth(parseISO(r.created_at)) === 8
+          ),
+      };
+    });
+
+    const result = await Promise.all(promise);
+
+    result.map(({ title, comments, reviews }) => {
+      if (comments.length + reviews.length === 0) {
+        return;
       }
-    );
 
-    console.table(
-      comments.data.map((r) => ({
-        user: r.user.login,
-        body: typeof r.body === 'string' ? r.body.substring(0, 64) : r.body,
-        created_at: r.created_at,
-        updated_at: r.updated_at,
-      }))
-    );
+      console.log(title);
 
-    const reviews = await octokit.request(
-      'GET /repos/{owner}/{repo}/pulls/{pull_number}/reviews',
-      {
-        owner,
-        repo,
-        pull_number: 71,
-        per_page: 100,
-      }
-    );
+      comments.length > 0 &&
+        console.table(
+          comments.map((r) => ({
+            body: typeof r.body === 'string' ? r.body.substring(0, 64) : r.body,
+            created_at: new Date(r.created_at).toLocaleString('pt-br'),
+          }))
+        );
 
-    console.table(
-      reviews.data.map((r) => ({
-        user: r.user.login,
-        body: typeof r.body === 'string' ? r.body.substring(0, 64) : r.body,
-        state: r.state,
-        submitted_at: r.submitted_at,
-      }))
-    );
+      reviews.length > 0 &&
+        console.table(
+          reviews.map((r) => ({
+            state: r.state,
+            body: typeof r.body === 'string' ? r.body.substring(0, 64) : r.body,
+            submitted_at: new Date(r.submitted_at).toLocaleString('pt-br'),
+          }))
+        );
+    });
   });
 })();
